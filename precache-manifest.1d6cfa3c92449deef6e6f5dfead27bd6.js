3a13638623ebc9fb53813d5c530a8f5652308fdd
Tal <talvindersingh1992@gmail.com>
2019-09-21 23:59:06 +0530

    Updates

---
+++ precache-manifest.1d6cfa3c92449deef6e6f5dfead27bd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0990118a70bd5a59f21fdb900d738de3",
+    "url": "/react-projects/supersquad/index.html"
+  },
+  {
+    "revision": "edc9fcb8e20348b452c4",
+    "url": "/react-projects/supersquad/static/css/main.b5978628.chunk.css"
+  },
+  {
+    "revision": "47708fc8d343bce8bc97",
+    "url": "/react-projects/supersquad/static/js/2.17f10642.chunk.js"
+  },
+  {
+    "revision": "edc9fcb8e20348b452c4",
+    "url": "/react-projects/supersquad/static/js/main.fc4eea9d.chunk.js"
+  },
+  {
+    "revision": "fbf20aeb51cb08557158",
+    "url": "/react-projects/supersquad/static/js/runtime-main.374431a5.js"
+  },
+  {
+    "revision": "4ddc758d490102bf391845e6f0791725",
+    "url": "/react-projects/supersquad/static/media/background.4ddc758d.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-projects/supersquad/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/supersquad/precache-manifest.6fc0b18670d3b70351dc20a983830754.js"
+  "/react-projects/supersquad/precache-manifest.1d6cfa3c92449deef6e6f5dfead27bd6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/supersquad/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-projects/supersquad/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
